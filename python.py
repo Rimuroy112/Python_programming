3fe16bcf1d7ec1a5b1c5056fdf379e6b1daf6df1
Rimu Roy <rimuroy112@DESKTOP-Q85H7H7.localdomain>
2025-06-17 00:56:52 +0600

    Added python.py script from notebook

---
+++ python.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello python")
+
+# In[2]:
+
+
+print("Hello again")
+
+# In[4]:
+
+
+print("Hello again and again")
+
+# In[ ]:
+
+
+
