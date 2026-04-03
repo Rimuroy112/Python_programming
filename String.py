613b268f27daf996a7ad116a5a0c5eceff73df83
Rimuroy112 <titashroy112@gmail.com>
2026-04-03 16:43:20 +0600

    String

---
+++ String.py
@@ -0,0 +1,14 @@
+str = "this is a python file.\nString"
+str2 = "bacd"
+print(str)
+print(str+str2) # Concatenation
+
+str3 = "robotics"
+print(str3[0:4]) # Slicing
+print(str3[-4:])
+
+print(str.endswith("ics"))
+print(str.capitalize())
+print(str.replace("python","c++"))
+print(str.find("c++"))
+print(str.count("s"))
