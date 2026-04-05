5bd2657f937dce92cdac00f6cf08fc57376b23ce
Rimuroy112 <titashroy112@gmail.com>
2026-04-05 16:52:22 +0600

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+my_dict = {
+    "name" : "abc",
+    "age" : 24,
+    "number" : 70,
+}
+print(my_dict)
+print(my_dict["age"])
+print(my_dict.keys())
+my_dict.update({"value":258})
+print(my_dict)
