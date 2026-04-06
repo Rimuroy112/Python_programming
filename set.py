e6c4d00973ade03be79b62c53f33fd9673d2cd92
Rimuroy112 <titashroy112@gmail.com>
2026-04-06 16:38:59 +0600

    Set

---
+++ set.py
@@ -0,0 +1,12 @@
+my_set = {1,2,3,4,2,3,3.4,5.6}
+print(my_set)
+my_set.add(6)
+my_set.add(7)
+print(my_set)
+my_set.remove(3.4)
+my_set.pop()
+print(my_set)
+set2 = {1,2,3}
+set3 = {2,3,4}
+print(set2.union(set3))
+print(set2.intersection(set3))
