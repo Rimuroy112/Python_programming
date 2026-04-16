46beeb4d2fa9b1e6ded2a22fb75471a540920cd4
Rimuroy112 <titashroy112@gmail.com>
2026-04-17 01:52:49 +0600

    Object oriented programmingg

--- OOP1.py
+++ OOP1.py
@@ -1,12 +1,13 @@
 class student:
     name = "Rahim"
-    def __init__(self,fullname):
-        self.name = fullname
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
         print(self)
         print("Adding new students in class: ")
 
-s1 = student("Karan")
-print(s1.name) 
+s1 = student("Karan",98)
+print(s1.name,s1.marks) 
 
-s2 = student("Arjun")
-print(s2.name) 
+s2 = student("Arjun", 99)
+print(s2.name,s2.marks) 
