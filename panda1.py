f079ca8c01f0405997b5bef25404745a1bd5d7a2
Rimuroy112 <titashroy112@gmail.com>
2026-04-24 18:09:43 +0600

    Panda

---
+++ panda1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+panda = pd.Series([1,2,3,4,5])
+panda.name = 'Robotics'
+panda.index = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+]
+print(panda.dtype)
+print(panda.values)
+print(type(panda.values))
+print(panda)
+print(panda['d'])
+panda2 = pd.Series({
+    'x':1,
+    'y':2,
+    'z':3,
+    'w':4,
+    'k':5},name='RobohuntersBRUR')
+print(panda2)
+print(panda2['y'])
+print(panda2.iloc[2])
+print(panda2[['w','k']])
