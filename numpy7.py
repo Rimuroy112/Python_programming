a00038f7ee5e25830e0568f780faa38c5a830028
Rimuroy112 <titashroy112@gmail.com>
2026-05-04 01:22:55 +0600

    Numpy

---
+++ numpy7.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr1 = np.array([1,2,3,4,5])
+arr2 = np.array([10,20,30,40,50])
+print(np.add(arr1, arr2))
+print(np.subtract(arr1, arr2))
+print(np.multiply(arr1, arr2))
+print(np.divide(arr1, arr2))
+arr_for_log = np.array([1, np.e, 100])
+print(arr_for_log)
+print(np.log(arr_for_log))
+print(np.log2(arr_for_log))
+print(np.log10(arr_for_log))
